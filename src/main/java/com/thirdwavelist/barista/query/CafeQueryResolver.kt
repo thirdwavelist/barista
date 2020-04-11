96a26d83e52943b65e08a2fd40240db45ac0f2c5
Antal János Monori <anthonymonori@gmail.com>
2020-04-12 00:02:59 +0200

    Fixed cafe query and removed unnecessary DTO class

--- src/main/java/com/thirdwavelist/barista/query/CafeQueryResolver.kt
+++ src/main/java/com/thirdwavelist/barista/query/CafeQueryResolver.kt
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class CafeQueryResolver(private val cafeService: CafeService) : GraphQLQueryResolver {
-    fun cafe(): Collection<Cafe> {
-        return cafeService.all
+
+    fun cafe(id: String?): Collection<Cafe> {
+        return if (!id.isNullOrBlank()) {
+            cafeService.getCafe(id)
+        } else {
+            cafeService.all
+        }
     }
 
-    fun cafe(id: String): Collection<Cafe> {
-        return cafeService.getCafe(id)
-    }
 }
