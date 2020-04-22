1672b9b7eb1d6a714ddb484353817a3dfe5d9571
Antal János Monori <anthonymonori@gmail.com>
2020-04-22 19:27:27 +0200

    Missing header config plus a config to force https

--- src/main/java/com/thirdwavelist/barista/config/SecurityConfig.kt
+++ src/main/java/com/thirdwavelist/barista/config/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package com.thirdwavelist.barista.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,13 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @Configuration
 open class SecurityConfig : WebSecurityConfigurerAdapter() {
-    @Value("\${webSecurity.requireSsl}")
-    private val requireHttps: Boolean = false
-
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        if (requireHttps) {
-            http.requiresChannel().anyRequest().requiresSecure()
-        }
+//        http.requiresChannel().anyRequest().requiresSecure()
     }
 }
